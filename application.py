937003c2765001466d84bf9d11f8222f3e127a3d
Snowfighter <chernayev.sergey.work@gmail.com>
2019-04-01 12:16:02 +0300

    Added sessions and SQLAlchemy support

--- application.py
+++ application.py
@@ -2,6 +2,7 @@ import os
 
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
+from flask_sqlalchemy import SQLAlchemy
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions
 from werkzeug.security import check_password_hash, generate_password_hash
@@ -28,3 +29,18 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+# Initiating db object
+db = SQLAlchemy(app)
+
+
+# Table class
+class Users(db.Model):
+    __tablename__ = 'users'
+    tab_id = db.Column('id', db.Integer, primary_key=True, nullable=False)
+    tab_username = db.Column('username', db.Text, nullable=False)
+    tab_hash = db.Column('hash', db.Text, nullable=False)
+
+    def __init__(self, tab_id, tab_username, tab_hash):
+        self.tab_id = tab_id
+        self.tab_username = tab_username
+        self.tab_hash = tab_hash
