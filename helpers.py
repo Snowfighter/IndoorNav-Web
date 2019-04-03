552563d1a68c515dbb245fb94ddb411acd8ae4d0
Snowfighter <chernayev.sergey.work@gmail.com>
2019-04-03 12:35:48 +0300

    Added decorator for a required login

---
+++ helpers.py
@@ -0,0 +1,14 @@
+from functools import wraps
+from flask import redirect, render_template, request, session
+
+
+# Route decorator for a required login
+# http://flask.pocoo.org/docs/0.12/patterns/viewdecorators/
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function()
+
